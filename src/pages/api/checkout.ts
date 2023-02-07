9335367db28a1805bfedd04f9c4bd5f4f4641ed1
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-02-06 22:26:40 -0300

    fix: added vercel env

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -24,8 +24,8 @@ export default async function handler(
     res.status(400).json({ error: "Products not found." });
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
-  const cancelUrl = `${process.env.NEXT_URL}/`;
+  const successUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
